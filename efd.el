3ec46f1b553c01008f6c5f164310af80a729d4b4
Jean-François Arbour <jean-francois@updata.ca>
2025-06-08 23:08:43 -0400

    feat: extract function calls from multiple files

--- efd.el
+++ efd.el
@@ -7,7 +7,7 @@
 ;; Created: May 30, 2025
 ;; Modified: May 30, 2025
 ;; Version: 0.0.1
-;; Keywords: convenience data docs extensions files help internal lisp local matching outlines processes tex text tools unix
+;; Keywords: convenience docs extensions files help internal lisp local matching outlines processes tex text tools
 ;; Homepage: https://github.com/jfa/efd
 ;; Package-Requires: ((emacs "24.3"))
 ;;
@@ -19,47 +19,64 @@
 ;;
 ;;; Code:
 
-(defun efd-compile-function-calls (file-path)
-  "Generate a hierarchical call graph for functions in FILE-PATH.
-Returns an alist where each entry is (FUNCTION . CALLED-FUNCTIONS)."
-  (let ((functions (efd-extract-functions file-path))
-        (function-calls '()))
-    (dolist (func functions)
-      (let ((calls (efd-find-function-calls func file-path functions)))
-        (push (cons (car func) calls) function-calls)))
-      function-calls))
+(defun efd-compile-function-calls (file-paths)
+  "Generate a list of function call for functions in FILE-PATHS.
+FILE-PATHS can be a single file path string or a list of file paths.
+Returns an alist of (FILE-PATH . FUNCTION-CALLS) where FUNCTION-CALLS
+is a list of (FUNC-NAME . (CALLEE1 CALLEE2 ...)) cons cells."
+  (let ((functions (efd-extract-functions file-paths))
+        (file-function-calls '()))
+    (dolist (func-info functions)
+      (let* ((file-path (car func-info))
+             (func-name (cadr func-info))
+             (calls (efd-find-function-calls func-info functions))
+             (file-entry (assoc file-path file-function-calls)))
+        (if file-entry
+            (setcdr file-entry (cons (cons func-name calls) (cdr file-entry)))
+          (push (cons file-path (list (cons func-name calls))) file-function-calls))))
+    file-function-calls))
 
-(defun efd-extract-functions (file-path)
-  "Extract all function definitions from FILE-PATH.
-Returns a list of (FUNCTION-NAME . START-POS) pairs."
+
+(defun efd-extract-functions (file-paths)
+  "Extract all function definitions from FILE-PATHS.
+FILE-PATHS can be a single file path string or a list of file paths.
+Returns a list of (FILE-PATH . (FUNC-NAME . FUNC-START)) tuples."
   (with-temp-buffer
-    (insert-file-contents file-path)
-    (goto-char (point-min))
-    (let ((functions '()))
-      (while (re-search-forward "^\\s-*(defun\\s-+\\([^[:space:]()]+\\)" nil t)
-        (let ((func-name (match-string 1))
-              (func-start (+ (match-beginning 0) 1)))
-          (push (cons func-name func-start) functions)))
+    (let ((functions '())
+          (files (if (listp file-paths) file-paths (list file-paths))))
+      (dolist (file-path files)
+        (let ((file-start (point-max)))
+          (goto-char (point-max))
+          (insert-file-contents file-path)
+          (goto-char file-start)
+          (while (re-search-forward "^\\s-*(defun\\s-+\\([^[:space:]()]+\\)" nil t)
+            (let ((func-name (match-string 1))
+                  (func-start (+ (match-beginning 0) 1)))
+              (push (cons file-path (cons func-name func-start)) functions)))))
       (nreverse functions))))
 
-(defun efd-find-function-calls (func-info file-path functions)
-  "Find all functions called within FUNC-INFO's definition in FILE-PATH.
-FUNC-INFO is a cons cell (FUNCTION-NAME . START-POS) as returned
-by `efd-extract-functions'.
-FUNCTIONS is a list of such cons cells for all function definitions in the file."
-  (with-temp-buffer
-    (insert-file-contents file-path)
-    (goto-char (cdr func-info))
-    (let ((func-end (efd-find-function-end))
-          (calls '()))
-      (while (and (< (point) func-end)
-                  (re-search-forward "(\\([^[:space:]()]+\\)" func-end t))
+(defun efd-find-function-calls (func-info functions)
+  "Find all functions called within FUNC-INFO's definition.
+FUNC-INFO is a tuple (FILE-PATH . (FUNC-NAME . FUNC-START)) as the entries
+in the list returned by `efd-extract-functions1'.
+FUNCTIONS is a list of such tuples."
+  (let ((file-path (car func-info))
+        (func-name (cadr func-info))
+        (func-start (cddr func-info))
+        (func-names (mapcar (lambda (entry) (cadr entry)) functions)))
+    (with-temp-buffer
+      (insert-file-contents file-path)
+      (goto-char func-start)
+      (let ((func-end (efd-find-function-end))
+            (calls '()))
+        (while (and (< (point) func-end)
+                    (re-search-forward "(\\([^[:space:]()]+\\)" func-end t))
           (let ((potential-call (match-string 1)))
-            (when (and (assoc potential-call functions)
-                       (not (string= potential-call (car func-info)))
+            (when (and (member potential-call func-names)
+                       (not (string= potential-call func-name))
                        (not (member potential-call calls)))
               (push potential-call calls))))
-      (nreverse calls))))
+        (nreverse calls)))))
 
 (defun efd-find-function-end ()
   "Find the end position of the current function definition."
@@ -75,9 +92,9 @@ OPTIONS is a plist that can contain:
   :edge-color - Color for edges
   :layout - Layout direction (TB, LR, BT, RL)"
   (let* ((graph-name (or (plist-get options :graph-name) "CallGraph"))
-         (node-color (or (plist-get options :node-color) "lightblue"))
+         (node-color (or (plist-get options :node-color) "lightgreen"))
          (edge-color (or (plist-get options :edge-color) "black"))
-         (layout (or (plist-get options :layout) "TB"))
+         (layout (or (plist-get options :layout) "LR"))
          (dot-lines '()))
     (push (format "digraph %s {" graph-name) dot-lines)
     (push (format "  rankdir=%s;" layout) dot-lines)
